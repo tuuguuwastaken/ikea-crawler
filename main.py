d5761cdca5fb3104e818c6b1ef819f4b4792a077
tuuguuwastaken <tuugii59704@gmail.com>
2022-08-02 10:24:06 +0800

    fixed

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def load_category (filename):
     category_wb = load_workbook('ikea-link.xlsx', read_only=True)
     category_sw = category_wb.active
     category_info = [[cell.value for cell in sub_category ] for sub_category in category_sw.rows]
+    category_info.append(category_info)
     return category_info
 
 def save_info(info, file_name):
@@ -25,13 +26,14 @@ def save_info(info, file_name):
 def get_info_from_url(browser, sub_category, url):
 
     products_info = []
-    browser.get(url + "?Page=200")
+    browser.get(url + '?page=200')
     time.sleep(30)
-    cards = browser.find_elements(By.CLASS_NAME,'withprice-item')
+    cards = browser.find_elements(By.CLASS_NAME,'product-overview-wrapper' or 'withprice-content ivu-col')
     for card in cards :
         product_name = card.find_element(By.CLASS_NAME, "withprice-title").text
         product_desc = card.find_element(By.CLASS_NAME, 'withprice-commit').text
         product_price = card.find_element(By.CLASS_NAME,"withprice-symbol").text + card.find_element(By.CLASS_NAME,'withprice-price').text
+        print(product_name, product_desc ,product_price)
         # product_url = card.find_element(By.TAG_NAME,"a").get_attribute('href')
         # product_code = product_url.split("-")[-1].strip("s/")
         products_images = [img_tag.get_attribute('src') for img_tag in card.find_elements(By.TAG_NAME, "img")]
@@ -39,7 +41,8 @@ def get_info_from_url(browser, sub_category, url):
                         product_desc, product_price,
                          len(products_images)]
         products_info += products_images
-        products_info.append(products_info)
+        # products_info.append(products_images)
+        product_info.append(products_info)
     return products_info
 
 
@@ -47,16 +50,18 @@ if __name__ == "__main__":
     path_driver = 'chromedriver.exe'
     driver = webdriver.Chrome(executable_path = path_driver)
     info = load_category('ikea-link.xlsx')
+    print(info)
     product_info = []
     i = 1
     for sub_category in info:
         print(f" checking and submitting {sub_category[0]} ({i}/{len(info)})")
-        product_info += get_info_from_url(driver, sub_category[0], sub_category[1])
+        product_info = get_info_from_url(driver, sub_category[0], sub_category[1])
         print(sub_category[0]+ ' ' + sub_category[1])
         i+=1
-    save_info(product_info, "products_catalog.xlsx")
+    # print(sub_category)
+    save_info(products_info, "products_catalog.xlsx")
+    print('saved')
     driver.close()
-
 
 
 
