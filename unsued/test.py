e238aa8e4c798708b170c6c44b1808dc190c22b9
tuuguuwastaken <tuugii59704@gmail.com>
2022-08-22 12:21:06 +0800

    update

--- unsued/test.py
+++
@@ -1,4 +0,0 @@
-import sympy
-
-if __name__ == '__main__':
-    print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
